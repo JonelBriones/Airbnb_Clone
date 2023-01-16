85e63510c467cb599d8caced96b198a13621954d
JonelBriones <jonel.c.briones@gmail.com>
2023-01-16 01:03:43 -0800

    added frontend

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import Banner from '@/components/Banner'
+import Footer from '@/components/Footer'
 import Header from '@/components/Header'
+import LargeCard from '@/components/LargeCard'
+import MediumCard from '@/components/MediumCard'
+import SmallCard from '@/components/SmallCard'
 import Head from 'next/head'
 
-export default function Home() {
+export default function Home({ exploreData, cardsData }) {
   return (
     <>
       <Head>
@@ -11,7 +16,55 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      {/* Banner */}
+      <Banner />
+
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+          {/* Pull some data from server end points */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData?.map(({ location, img, distance }) => (
+              <SmallCard
+                key={img}
+                img={img}
+                location={location}
+                distance={distance}
+              />
+            ))}
+          </div>
+        </section>
+        <section>
+          <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+          {/* Medium Cards */}
+          <div className="flex space-x-3 overflow-scroll overflow-y-hidden p-3 -ml-3">
+            {cardsData?.map(({ img, title }) => (
+              <MediumCard key={img} img={img} title={title} />
+            ))}
+          </div>
+        </section>
+        <LargeCard
+          image="https://links.papareact.com/4cj"
+          title="The Greatest Outdoors"
+          description="Wishlists curated by Airbnb."
+          buttonText="Get Inspired"
+        />
+      </main>
+      <Footer />
     </>
   )
+}
+// Render static images
+export async function getStaticProps() {
+  const exploreData = await fetch('https://www.jsonkeeper.com/b/4G1G').then(
+    (res) => res.json()
+  )
+  const cardsData = await fetch('https://www.jsonkeeper.com/b/VHHT').then(
+    (res) => res.json()
+  )
+  return {
+    props: {
+      exploreData,
+      cardsData,
+    },
+  }
 }
